3fe77d7081d04b51819899d1c71342f353f6b17e
Sebastien Rannou <srannou@ocs.online.net>
2015-03-31 14:47:25 +0200

    Fixed do-release script.

--- do-release.bash
+++ do-release.bash
@@ -3,9 +3,10 @@
 # A tiny helper to release the binary under the dist branch.
 
 docker build -t 'xnbd-client-static' .
-docker cp xnbd-client-static /xnbd-client-static .
 git checkout origin/dist -b dist
-git add xnbd-client-static
+docker run --name xnbd-client-static xnbd-client-static /bin/true
+docker cp xnbd-client-static:/xnbd-client-static dist
+git add dist/xnbd-client-static
 git commit -am "Bumped new release."
 git push origin dist
 git checkout master
